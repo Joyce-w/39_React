e4b22124f9b9a21c6a174b800a7601384bd59038
Joyce <hellojoycewong@gmail.com>
2021-06-05 15:28:28 -0700

    Refactor conditionals for voting text

--- 39.2_JSX_Exercises/part-3/Person.js
+++ 39.2_JSX_Exercises/part-3/Person.js
@@ -1,14 +1,15 @@
 const Person = (props) => {
     let age = props.age;
+    let voteText = age > 18 ? "Please go vote!" : "You must be 18"
     let name = props.name;
 
     return (
         <div>
         <p>Learn some information about this person</p>
             <ul>
-                <li> Name: { name.length > 8 ? name.slice(0,6): name} </li>
+                <li> Name: { name.slice(0,6)} </li>
                 <li> Age: {props.age} </li>
-                <h3>{age > 18 ? "Please go vote!" : "You must be 18"}</h3>
+                <h3>{voteText}</h3>
             </ul>
             
             <ul>
