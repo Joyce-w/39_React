108ae82c84b0049edb1a38d5f5b0f9bca2f962da
Joyce <hellojoycewong@gmail.com>
2021-06-08 17:36:46 -0700

    Add color counter

--- 39.4_8_ball/src/EightBall.js
+++ 39.4_8_ball/src/EightBall.js
@@ -11,23 +11,55 @@ const EightBall = ({ answers }) => {
     const [msg, newAnswer] = useState("Think of a Question")
     //state for changing colors
     const [bgColor, newColor] = useState("black")
-    
+
+    //state for counting green
+    const [greenCount, addGreen] = useState(0)
+    //state for counting goldenrod
+    const [goldenrodCount, addGoldenrod] = useState(0)
+    //state for counting red
+    const [redCount, addRed] = useState(0)
+
     //update the state value each time the div is clicked
     const click = () => {
+        
         changeAnswerNum(rand())
         newAnswer(answers[answerNum].msg)
         newColor(answers[answerNum].color)
+        counting(bgColor)
     }
 
     //reset the eight ball back to default values
     const reset = () => {
         newColor('black')
         newAnswer('Think of a Question')
+        addGreen(0)
+        addGoldenrod(0)
+        addRed(0)
     }
 
+    let counting = (bgColor) => {
+        console.log(bgColor)
+        if (bgColor === 'green') {
+            addGreen(greenCount + 1)
+            
+        }
+        if (bgColor === 'red') {
+            addRed(redCount + 1)
+        }
+        if (bgColor === 'goldenrod') {
+            addGoldenrod(goldenrodCount + 1)
+        }
+    }
     return(
         <>
             <h1 > EightBall</h1>
+            <div>
+                <p>Red: { redCount }</p>
+                <p>Green: { greenCount }</p>
+                <p>goldenrod: { goldenrodCount } </p>
+                
+                
+            </div>
             <div onClick={() => click()} style={{ backgroundColor: bgColor, borderColor: bgColor }} className="EightBall">
                 <p className="EightBall-p">{msg}</p>
             </div>
@@ -37,3 +69,16 @@ const EightBall = ({ answers }) => {
 }
 
 export default EightBall;
+
+
+/*Soln shows the call back function returning entire div:
+
+  return (
+    <div
+      className="EightBall"
+      onClick={handleClick}
+      style={{ backgroundColor: color }}
+    >
+      <b>{msg}</b>
+    </div>
+  );*/
