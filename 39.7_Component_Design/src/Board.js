3354cb7abc28945e462094bb1ea610d3df7364bc
Joyce <hellojoycewong@gmail.com>
2021-06-21 12:03:51 -0700

    change tile to react to click

--- 39.7_Component_Design/src/Board.js
+++ 39.7_Component_Design/src/Board.js
@@ -27,16 +27,16 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows = 5, ncols = 4, chanceLightStartsOn = 0.4 }) {
+function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.4 }) {
   const [board, setBoard] = useState(createBoard(nrows, ncols));
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard(nrows, ncols) {
     let initialBoard = [];
     // TODO: create array-of-arrays of true/false values
-    for (let i = 0; i < ncols; i++) {
+    for (let i = 0; i < nrows; i++) {
       let row = []
       initialBoard.push(row)
-      for (let j = 0; j < nrows; j++) {
+      for (let j = 0; j < ncols; j++) {
         row.push(Math.random() < chanceLightStartsOn ? true : false)
       }
     }
@@ -44,33 +44,6 @@ function Board({ nrows = 5, ncols = 4, chanceLightStartsOn = 0.4 }) {
   }
 
   console.log(board)
-  // function createBoard(nrows, ncols) {
-  //   let initialBoard = [];
-  //   // TODO: create array-of-arrays of true/false values
-  //   for (let r = 0; r < nrows; r++) {
-  //     let row = []
-  //     for (let c = 0; c < ncols; c++) {
-  //       //Create random num,  0 === false , 1 === true
-  //       const rand = () => {
-  //         let num = (Math.random() * 1)
-  //         return num;
-  //       }
-  //       row.push(
-  //                 <Cell
-  //           key={`${r}-${c}`} coord={`${r}-${c}`} flipCellsAroundMe={() => flipCellsAround(`${r}-${c}`)}
-  //           isLit={rand() < chanceLightStartsOn ? false : true}
-  //         /> 
-  //       )
-  //     }
-  //     initialBoard.push(
-  //       <tr>
-  //         {row}
-  //       </tr>
-  //     )
-
-  //   }
-  //   return initialBoard;
-  // }
   
   function hasWon(stateBoard) {
     // TODO: check the board in state to determine whether the player has won.
@@ -86,32 +59,34 @@ function Board({ nrows = 5, ncols = 4, chanceLightStartsOn = 0.4 }) {
   function flipCellsAround(coord) {
     setBoard(oldBoard => {
       const [y, x] = coord.split("-").map(Number);
-
+      console.log(y, x)
+      
       const flipCell = (y, x, boardCopy) => {
         // if this coord is actually on board, flip it
-
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
           boardCopy[y][x] = !boardCopy[y][x];
         }
+
       };
 
       // TODO: Make a (deep) copy of the oldBoard
       const copyOldBoard = [...board]
       // TODO: in the copy, flip this cell and the cells around it
+      flipCell(y, x, copyOldBoard)
 
       // TODO: return the copy
+      return copyOldBoard
     });
   }
 
   // if the game is won, just show a winning msg & render nothing else
 
-  // TODO
 
   // make table board
   let tableBoard = [];
-  for (let y = 0; y < ncols; y++) {
+  for (let y = 0; y < nrows; y++) {
     let row = [];
-    for (let x = 0; x < nrows; x++) {
+    for (let x = 0; x < ncols; x++) {
       let coord = `${y}-${x}`
       row.push(
         <Cell
@@ -121,13 +96,9 @@ function Board({ nrows = 5, ncols = 4, chanceLightStartsOn = 0.4 }) {
         />
       )
     }
-    tableBoard.push(<tr key={y}> { row } </tr>)
+    tableBoard.push(<tr key={y}>{row}</tr>)
   }
 
-
-  console.log(tableBoard)
-   
-  
 
   return (
     <table>
