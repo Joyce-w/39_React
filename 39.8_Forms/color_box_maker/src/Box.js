4da56c4293d59864d6eb6c1a4ee795a8995302d8
Joyce <hellojoycewong@gmail.com>
2021-06-23 15:24:31 -0700

    Add functional remove() to boxes when clicked

--- 39.8_Forms/color_box_maker/src/Box.js
+++ 39.8_Forms/color_box_maker/src/Box.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Box.css"
 
 
-const Box = () => {
+const Box = ({id, boxWidth, boxHeight, bgColor, remove}) => {
+    
+    const divStyle = {
+        width: +boxWidth,
+        height: +boxHeight,
+        backgroundColor: bgColor
+    }
+
+    return (
+        <>
+            <div id={ id }className="Box" style={divStyle}>
+            <button className="Box-btn" onClick={remove}>x</button>
+            </div>
+        </>
+    )
 
 }
 
--- 39.8_Forms/color_box_maker/src/BoxList.js
+++ 39.8_Forms/color_box_maker/src/BoxList.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid"
 import Box from "./Box"
 import NewBoxForm from "./NewBoxForm"
 import './BoxList.css'
@@ -7,28 +8,48 @@ const BoxList = () => {
 
     const INITIAL_STATE = [
         {
-            width: '30',
-            height: '50',
+            id: uuid(),
+            width: 30,
+            height: 50,
             bgColor: '#ffa3a3'
+        },
+        {
+            id: uuid(),
+            width: 90,
+            height: 90,
+            bgColor: "#cfccff"
         }
     ]
     //state for box properties
     const [ boxData, setBoxData ] = useState(INITIAL_STATE)
-    console.log(boxData)
 
     //create functional prop to pass down to children
     const addBox = ( width, height, bgColor ) => {
-        setBoxData((boxData) => [...boxData, { width, height, bgColor }])
+        setBoxData((boxData) => [...boxData, { width, height, bgColor, id:uuid() }])
     }
+    
+    //remove box on click
+    const remove = (e) => {
+        let parentBoxID = e.target.parentElement.id
 
+        //filter data to update boxData without the removed box
+        setBoxData(boxData.filter(({id}) => {
+            return id !== parentBoxID;
+        }))
+    }
 
     return (
         <div className="BoxList">
             <h1>BoxList</h1>
-            <NewBoxForm addBox={ addBox }/>
-        </div>
-        
+            <NewBoxForm addBox={addBox} />
+            
+            <div className="BoxList-Boxes">
+                {boxData.map(({ id, width, height, bgColor }) => {
+                    return <Box key={id} id={ id }boxWidth={width} boxHeight={height} bgColor={bgColor} remove={ remove }/>
+                })}
+            </div>
 
+        </div>
     )
 }
 
