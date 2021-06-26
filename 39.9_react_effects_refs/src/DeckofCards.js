466d3293a5e517123e744f846d4caae46759daeb
Joyce <hellojoycewong@gmail.com>
2021-06-25 18:28:45 -0700

    implement timer, need fixing

--- 39.9_react_effects_refs/src/DeckofCards.js
+++ 39.9_react_effects_refs/src/DeckofCards.js
@@ -1,4 +1,4 @@
-import React, {useState,  useEffect } from "react"
+import React, {useState,  useEffect, useRef } from "react"
 import axios from "axios"
 import Card from "./Card"
 import "./DeckofCards.css"
@@ -8,22 +8,9 @@ const DeckofCards = () => {
     
     const [deckID, setDeckID] = useState()
     const [card, setCard] = useState()
-    console.log(card)
+    // console.log(card)
 
-    async function grabCard() {
-
-        const deck = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
-        let newCard = deck.data;
-
-        if (newCard.remaining === 0) {
-            alert("Last card")
-            setCard(null)
-        } else {
-            setCard(newCard);
-        }
-
-    }       
-
+    //add effect to render a deck id after page loads
     useEffect(() => {
         //grab a new deck
         async function newDeck() {
@@ -37,17 +24,52 @@ const DeckofCards = () => {
         newDeck();
     },[])
     
+    //function to grab a card when page loads
+    async function grabCard() {
+        const deck = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+        let newCard = deck.data;
+
+        if (newCard.remaining === 0) {
+            alert("Last card")
+            setCard(null)
+        } else {
+            setCard(newCard);
+        }
+    }
+    
+    const [drawing, setDrawing] = useState(false);
+
+    const [seconds, setSeconds] = useState(0)
+
+    const timerId = useRef();
+    console.log(timerId)
+
+    //trigger timer when clicked
+    useEffect(() => {
+        timerId.current = setInterval(() => {
+            setSeconds(seconds => seconds + 1)
+            grabCard();
+        }, 1000)
+
+    return clearInterval(timerId.current)
+
+    },[])
+
+    const stopTimer = () => {
+        clearInterval(timerId.current)
+    }
+
     return (
  
         <div className="DeckofCards">
             <button className="DeckofCards-btn" onClick={() => grabCard()}> Draw Card </button>
 
-            <div className="DeckofCards-card" >
+            <div className="DeckofCards-card">
                 { card ? <Card card={card.cards[0].image} /> : "Loading..."}                  
             </div>
 
+            <button onClick={ stopTimer }>Drawing</button>
         </div>
-
 
     )
 }
